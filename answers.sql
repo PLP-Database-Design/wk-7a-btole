5412fdc4f512d14723c8be49514118c2436b1491
btole <74013671+btole@users.noreply.github.com>
2025-05-13 10:43:43 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -6,3 +6,30 @@ JOIN (SELECT 1 AS n UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5)
   ON CHAR_LENGTH(Products)
      -CHAR_LENGTH(REPLACE(Products, ',', '')) >= n.n - 1;
 
+
+Question 2
+
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100) NOT NULL
+);
+
+
+CREATE TABLE OrderItems (
+    OrderID INT,
+    Product VARCHAR(50),
+    Quantity INT NOT NULL,
+    PRIMARY KEY (OrderID, Product),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+
+
+INSERT INTO Orders (OrderID, CustomerName)
+SELECT DISTINCT OrderID, CustomerName
+FROM OrderDetails;
+
+
+INSERT INTO OrderItems (OrderID, Product, Quantity)
+SELECT OrderID, Product, Quantity
+FROM OrderDetails;
+
