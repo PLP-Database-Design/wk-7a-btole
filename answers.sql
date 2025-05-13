612414b00a8953b54b1801395518cee59d04106f
btole <74013671+btole@users.noreply.github.com>
2025-05-13 10:41:30 +0300

    Update answers.sql

---
+++ answers.sql
@@ -0,0 +1,8 @@
+Question 1
+
+SELECT OrderID, CustomerName, TRIM(SUBSTRING_INDEX(SUBSTRING_INDEX(Products, ',', n.n), ',', -1)) AS Product
+FROM ProductDetail
+JOIN (SELECT 1 AS n UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5) n
+  ON CHAR_LENGTH(Products)
+     -CHAR_LENGTH(REPLACE(Products, ',', '')) >= n.n - 1;
+
